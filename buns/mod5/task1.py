d59bb6fc9b4b6ee0f0807f6bec0e5995782e47b1
hothalfbloodprince <129889352+hothalfbloodprince@users.noreply.github.com>
2023-10-25 22:49:10 +0500

    Add files via upload

---
+++ buns/mod5/task1.py
@@ -0,0 +1,57 @@
+class Node:
+    """
+    Вспомогательный класс для узлов списка
+    """
+    def __init__(self, data):
+        self.data = data  # храним информацию
+        self.pref = None  # ссылка на предыдущий узел
+
+class Stack:
+    """
+    Основной класс для стека
+    """
+
+    def __init__(self):
+        self.end = None  # ссылка на конец стека
+
+    def pop(self):
+        """
+        возвращение последнего элемента из списка с удалением его из списка
+        """
+        if self.end is None:
+            return None
+        else:
+            val = self.end.data
+            self.end = self.end.pref
+            return val
+
+    def push(self, val):
+        """
+        добавление элемента val в конец списка
+        """
+        new_node = Node(val)
+        if self.end is None:
+            self.end = new_node
+        else:
+            new_node.pref = self.end
+            self.end = new_node
+
+    def print(self):
+        """
+        вывод на печать содержимого стека
+        """
+        current = self.end
+        while current:
+            print(current.data)
+            current = current.pref
+"""
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.print()
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+"""
